14ced2d8beaa630f668fa636259fb860b628a852
Gonzalez <diego.x.gonzalez@sabre.com>
2024-07-16 13:41:11 -0300

    Entrega 1

--- main.js
+++ main.js
@@ -1,76 +1,81 @@
 const songs = [
-    {
-      title: "Baby One More Time",
-      artist: "Britney Spears",
-      src: "./media/musica/Britney Spears - Baby One More Time.mp4",
-    },
-    {
-      title: "Genie in a Bottle",
-      artist: "Christina Aguilera",
-      src: "./media/musica/Christina Aguilera - Genie In A Bottle.mp4",
-    },
-    {
-      title: "Basket Case",
-      artist: "Green Day",
-      src: "./media/musica/Green Day - Basket Case.mp4",
-    },
-    {
-      title: "Smell Like a Teen Spirit",
-      artist: "Nirvana",
-      src: "./media/musica/Nirvana - Smells Like Teen Spirit.mp4",
-    },
-    {
-      title: "Wonderwall",
-      artist: "Oasis",
-      src: "./media/musica/Oasis - Wonderwall.mp4",
-    },
-    {
-      title: "Wannabe",
-      artist: "Spice Girls",
-      src: "./media/musica/Spice Girls - Wannabe.mp4",
-    },
-    {
-      title: "Under the Bridge",
-      artist: "Red Hot Chili Peppers",
-      src: "./media/musica/Red Hot Chili Peppers - Under The Bridge.mp4",
-    },
-  ];
-  
-  let cancion = null;
+  {
+    title: "Baby One More Time",
+    artist: "Britney Spears",
+    src: "./media/musica/Britney Spears - Baby One More Time.mp4",
+  },
+  {
+    title: "Genie in a Bottle",
+    artist: "Christina Aguilera",
+    src: "./media/musica/Christina Aguilera - Genie In A Bottle.mp4",
+  },
+  {
+    title: "Basket Case",
+    artist: "Green Day",
+    src: "./media/musica/Green Day - Basket Case.mp4",
+  },
+  {
+    title: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    src: "./media/musica/Nirvana - Smells Like Teen Spirit.mp4",
+  },
+  {
+    title: "Wonderwall",
+    artist: "Oasis",
+    src: "./media/musica/Oasis - Wonderwall.mp4",
+  },
+  {
+    title: "Wannabe",
+    artist: "Spice Girls",
+    src: "./media/musica/Spice Girls - Wannabe.mp4",
+  },
+  {
+    title: "Under the Bridge",
+    artist: "Red Hot Chili Peppers",
+    src: "./media/musica/Red Hot Chili Peppers - Under The Bridge.mp4",
+  },
+];
+
+function adivinarcancion() {
   let intentos = 0;
   let currentTime = 0;
   let audio = null;
-  
-  function startGame() {
+  let cancion = null;
+  let timeoutId = null;
+
+  function comenzar() {
     intentos = 0;
     currentTime = 0;
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
     audio = null;
-  
     cancion = songs[Math.floor(Math.random() * songs.length)];
     alert(`Artista: ${cancion.artist}`);
     reproducir();
   }
-  
+
   function reproducir() {
     if (!audio) {
       audio = new Audio(cancion.src);
     }
     audio.currentTime = currentTime;
     audio.play();
-  
-    setTimeout(() => {
+
+    timeoutId = setTimeout(() => {
       audio.pause();
       currentTime = audio.currentTime;
       preguntatitulo();
     }, 5000);
   }
-  
+
   function preguntatitulo() {
-    const title = prompt("¿Cual es el nombre de la canción?");
+    const title = prompt("¿Cuál es el nombre de la canción?");
     if (title.toLowerCase() === cancion.title.toLowerCase()) {
-      alert("Muy bien");
+      alert("¡Muy bien!");
       if (confirm("¿Te gustaría jugar de nuevo?")) {
-        startGame();
+        comenzar();
       }
     } else {
       intentos++;
@@ -80,14 +85,16 @@ const songs = [
       } else {
         alert("Perdiste =(");
         if (confirm("¿Te gustaría jugar de nuevo?")) {
-          startGame();
+          comenzar();
         }
       }
     }
   }
-  
-  window.onload = function () {
-    const startButton = document.getElementById("startButton");
-    startButton.onclick = startGame;
-  };
-  
+
+  comenzar();
+}
+
+window.onload = function () {
+  const comenzarBtn = document.getElementById("bcomenzar");
+  comenzarBtn.onclick = adivinarcancion;
+};
